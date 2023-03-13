624746590447adaa9aade5772043a83f507e11ea
Sergio García <sergiogarcia@iMac-de-Sergio.local>
2023-03-13 11:20:22 +0100

    Finalizacion apartado 1 del Menú del Trabajador

--- NEWSUPERMERCADO/src/NewSupermercado/Supermercado.java
+++ NEWSUPERMERCADO/src/NewSupermercado/Supermercado.java
@@ -8,7 +8,7 @@ public class Supermercado {
 	
 	private String nombre;
 	private int stock;
-	private ArrayList <Producto> productos;
+	private static ArrayList<Producto> productos;
 	
 	
 	public Supermercado(String nombre) {
@@ -21,77 +21,50 @@ public class Supermercado {
 		
 		ArrayList <Producto> productos = new ArrayList <Producto>();
 		
-		Producto Lomo = new Producto("Lomo", 15, Categoria.CARNICERIA);
-		Producto Jamon = new Producto("Jamón", 10, Categoria.CARNICERIA);
-		Producto Cordero = new Producto("Cordero", 10, Categoria.CARNICERIA);
-		Producto Chorizo = new Producto("Chorizo", 20, Categoria.CARNICERIA);
-		Producto Salchicha = new Producto("Salchicha", 30, Categoria.CARNICERIA);
+		productos.add(new Producto("Lomo", 15, Categoria.CARNICERIA));
+		productos.add(new Producto("Jamón", 10, Categoria.CARNICERIA));
+		productos.add(new Producto("Cordero", 10, Categoria.CARNICERIA));
+		productos.add(new Producto("Chorizo", 20, Categoria.CARNICERIA));
+		productos.add(new Producto("Salchicha", 30, Categoria.CARNICERIA));
 
-		Producto Agua = new Producto("Agua", 20, Categoria.BEBIDAS);
-		Producto CocaCola = new Producto("CocaCola", 10, Categoria.BEBIDAS);
-		Producto Cerveza = new Producto("Cerveza", 25, Categoria.BEBIDAS);
-		Producto Fanta = new Producto("Fanta", 15, Categoria.BEBIDAS);
-		Producto Ron = new Producto("Ron", 10, Categoria.BEBIDAS);
+		productos.add(new Producto("Agua", 20, Categoria.BEBIDAS));
+		productos.add(new Producto("CocaCola", 10, Categoria.BEBIDAS));
+		productos.add(new Producto("Cerveza", 25, Categoria.BEBIDAS));
+		productos.add(new Producto("Fanta", 15, Categoria.BEBIDAS));
+		productos.add(new Producto("Ron", 10, Categoria.BEBIDAS));
 		
-		Producto Manzana = new Producto("Manzana", 15, Categoria.FRUTERIA);
-		Producto Platano = new Producto("Platano", 35, Categoria.FRUTERIA);
-		Producto Pina = new Producto("Pina", 20, Categoria.FRUTERIA);
-		Producto Fresa = new Producto("Fresa", 31, Categoria.FRUTERIA);
-		Producto Melocoton = new Producto("Melocoton", 23, Categoria.FRUTERIA);
+		productos.add(new Producto("Manzana", 15, Categoria.FRUTERIA));
+		productos.add(new Producto("Platano", 35, Categoria.FRUTERIA));
+		productos.add(new Producto("Pina", 20, Categoria.FRUTERIA));
+		productos.add(new Producto("Fresa", 31, Categoria.FRUTERIA));
+		productos.add(new Producto("Melocoton", 23, Categoria.FRUTERIA));
 		
-		Producto Actimel = new Producto("Actimel", 20, Categoria.LÁCTEOS);
-		Producto Danonino = new Producto("Danonino", 30, Categoria.LÁCTEOS);
-		Producto BiFrutas = new Producto("BiFrutas", 12, Categoria.LÁCTEOS);
-		Producto Danacol = new Producto("Danacol", 20, Categoria.LÁCTEOS);
-		Producto Yogurt = new Producto("Yogurt", 30, Categoria.LÁCTEOS);
+		productos.add(new Producto("Actimel", 20, Categoria.LÁCTEOS));
+		productos.add(new Producto("Danonino", 30, Categoria.LÁCTEOS));
+		productos.add(new Producto("BiFrutas", 12, Categoria.LÁCTEOS));
+		productos.add(new Producto("Danacol", 20, Categoria.LÁCTEOS));
+		productos.add(new Producto("Yogurt", 30, Categoria.LÁCTEOS));
 		
-		Producto Dorada = new Producto("Dorada", 10, Categoria.PESCADERIA);
-		Producto Percebes = new Producto("Dorada", 85, Categoria.PESCADERIA);
-		Producto Lubina = new Producto("Dorada", 5, Categoria.PESCADERIA);
-		Producto Langostinos = new Producto("Dorada", 100, Categoria.PESCADERIA);
-		Producto Gambones = new Producto("Dorada", 60, Categoria.PESCADERIA);
+		productos.add(new Producto("Dorada", 10, Categoria.PESCADERIA));
+		productos.add(new Producto("Lubina", 85, Categoria.PESCADERIA));
+		productos.add(new Producto("Gambas", 5, Categoria.PESCADERIA));
+		productos.add(new Producto("Gambones", 100, Categoria.PESCADERIA));
+		productos.add(new Producto("Nécoras", 60, Categoria.PESCADERIA));
 		
-		Producto Estropajo = new Producto("Estropajo", 20, Categoria.HOGAR);
-		Producto Fairy = new Producto("Fairy", 30, Categoria.HOGAR);
-		Producto Esponja = new Producto("Esponja", 20, Categoria.HOGAR);
-		Producto Balleta = new Producto("Balleta", 20, Categoria.HOGAR);
-		Producto Cubiertos = new Producto("Cubiertos", 30, Categoria.HOGAR);
-		
-		productos.add(Lomo);
-		productos.add(Jamon);
-		productos.add(Cordero);
-		productos.add(Chorizo);
-		productos.add(Salchicha);
-		productos.add(Agua);
-		productos.add(CocaCola);
-		productos.add(Cerveza);
-		productos.add(Fanta);
-		productos.add(Ron);
-		productos.add(Manzana);
-		productos.add(Platano);
-		productos.add(Pina);
-		productos.add(Fresa);
-		productos.add(Melocoton);
-		productos.add(Actimel);
-		productos.add(Danonino);
-		productos.add(BiFrutas);
-		productos.add(Danacol);
-		productos.add(Yogurt);
-		productos.add(Dorada);
-		productos.add(Percebes);
-		productos.add(Lubina);
-		productos.add(Langostinos);
-		productos.add(Gambones);
-		productos.add(Estropajo);
-		productos.add(Fairy);
-		productos.add(Esponja);
-		productos.add(Balleta);
-		productos.add(Cubiertos);
-		
+		productos.add(new Producto("Estropajo", 20, Categoria.HOGAR));
+		productos.add(new Producto("Fairy", 30, Categoria.HOGAR));
+		productos.add(new Producto("Esponja", 20, Categoria.HOGAR));
+		productos.add(new Producto("Balleta", 20, Categoria.HOGAR));
+		productos.add(new Producto("Cubiertos", 30, Categoria.HOGAR));
+	
 		
 		return productos;
 		
 	}
+	
+	
+	
+	
 	
 	public Supermercado(ArrayList<Producto> productos, int stock) {
 	
@@ -99,7 +72,9 @@ public class Supermercado {
 		this.stock = stock;
 	}
 	
-	public ArrayList<Producto> getProductos() {
+
+
+	public List<Producto> getProductos() {
 		
 		return productos;
 	}
@@ -114,6 +89,20 @@ public class Supermercado {
 		this.stock = stock;
 	}
 	
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
 	public void agregarProducto(Producto producto) {
 		
 	}
@@ -125,6 +114,8 @@ public class Supermercado {
 	public void buscarProducto(String nombreProducto) {
 		
 	}
+
+
 
 
 	
--- NEWSUPERMERCADO/src/NewSupermercado/SupermercadoMain.java
+++ NEWSUPERMERCADO/src/NewSupermercado/SupermercadoMain.java
@@ -2,12 +2,24 @@ package NewSupermercado;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class SupermercadoMain {
 	
 	static Scanner entrada = new Scanner(System.in);
 	static Supermercado Javadona = new Supermercado("Javadona");
+	static ArrayList <Producto> listaProductos = Javadona.productosIniciales();
+	public static void main(String [] args) {
+
+		
+		menuInicial();
+		
+	
+
+	}
+	
 	public static void menuInicial() {	
 
 		System.out.println("**********MENÚ**********" );												   
@@ -55,7 +67,7 @@ public class SupermercadoMain {
 		
 		System.out.println("Introduce tu elección en el siguiente menú: ");
 		System.out.println("**********MENÚ**********");
-		System.out.println("1.Mostrar productos del Supermercado");
+		System.out.println("1.Mostrar productos del Supermercado por categoría");
 		System.out.println("2.Añadir productos");
 		System.out.println("3.Eliminar productos");
 		System.out.println("4.Modificar productos");
@@ -70,8 +82,8 @@ public class SupermercadoMain {
 			
 			switch (respuesta) {
 			case 1: {
+				
 				menuOpcion1Trabajador();
-			
 				
 				
 				break;
@@ -110,6 +122,12 @@ public class SupermercadoMain {
 		
 	}
 	
+
+	
+	
+	
+	
+	
 	public static void menuOpcion1Trabajador() {
 		System.out.println("Introduzca la categoría del producto: ");
 		System.out.println("1.FRUTERIA");
@@ -122,44 +140,64 @@ public class SupermercadoMain {
 		int eleccion = entrada.nextInt();
 		
 		
-		if (eleccion < 1 || eleccion > 5) {
+		if (eleccion < 1 || eleccion > 6) {
 			System.err.println("Elección incorrecta, volviendo al menú inicial...");
 			menuTrabajador();
 		} else {
 				
+	
 			switch (eleccion) {
 			case 1:
-				Javadona.getProductos();
-				//BUSCAR COMO MOSTRAR LOS PRODUCTOS DE LAS LISTA
+				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.FRUTERIA)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}
 				break;
 			case 2:
-						 
-						
+				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.CARNICERIA)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}	 	
 				break;
 			case 3:
-				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.PESCADERIA)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}
 				break;
 			case 4:
-				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.HOGAR)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}
 				break;
 			case 5:
-				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.BEBIDAS)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}
 				break;
 			case 6:
-				
+				for (int i = 0; i < listaProductos.size(); i++) {
+					if (listaProductos.get(i).getCategoria().equals(Categoria.LÁCTEOS)) {
+						System.out.println(listaProductos.get(i).getNombre());
+					}	
+				}
 				break;
 
-			default:
+			default: 
+				System.err.println("Opción no válida.");
 				break;
 			}
 		}
 	}
-	public static void main(String [] args) {
 
-		
-		menuInicial();
-		
-
-	}
 
 }
